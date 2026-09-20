36aa29326f36ae5a112c61d69218e7740c71eff8
João Barreira <jovi2000barreira@gmail.com>
2026-09-20 20:00:29 -0300

    feat: implement PDF ingestion and embedding generation

--- src/ingest.py
+++ src/ingest.py
@@ -1,13 +1,47 @@
 import os
+
 from dotenv import load_dotenv
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_openai import OpenAIEmbeddings
+from langchain_postgres import PGVector
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 load_dotenv()
 
-PDF_PATH = os.getenv("PDF_PATH")
+PDF_PATH = os.getenv("PDF_PATH", "document.pdf")
+DATABASE_URL = os.getenv("DATABASE_URL")
+COLLECTION_NAME = os.getenv("PG_VECTOR_COLLECTION_NAME", "rag_collection")
+EMBEDDING_MODEL = os.getenv("OPENAI_EMBEDDING_MODEL", "text-embedding-3-small")
+
 
 def ingest_pdf():
-    pass
+    if not DATABASE_URL:
+        raise ValueError("DATABASE_URL not defined in .env")
 
+    print(f"Loading PDF: {PDF_PATH}")
+    loader = PyPDFLoader(PDF_PATH)
+    documents = loader.load()
+    print(f"{len(documents)} pages loaded.")
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=150,
+    )
+    chunks = text_splitter.split_documents(documents)
+    print(f"Document splited into {len(chunks)} chunks.")
+
+    embeddings = OpenAIEmbeddings(model=EMBEDDING_MODEL)
+    print(f"Generating embeddings with the model '{EMBEDDING_MODEL}'...")
+
+    print(f"Saving vectors in the collection '{COLLECTION_NAME}'...")
+    PGVector.from_documents(
+        documents=chunks,
+        embedding=embeddings,
+        collection_name=COLLECTION_NAME,
+        connection=DATABASE_URL,
+    )
+
+    print("Ingestion completed successfully!")
 
 if __name__ == "__main__":
     ingest_pdf()
